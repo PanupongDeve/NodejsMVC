26815d27e9cee6cbdb419cb15b7cdb1c604c6d5c
PanupongDeve <5635512110@psu.ac.th>
2017-11-10 14:26:12 +0700

    Add ENV CODE

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const app = require('./app');
 
-app.listen(3050, () => {
-    console.log('Running on port 3050');
+let PORT = 3000;
+
+if (process.env.NODE_ENV === 'production') {
+    PORT = 80;
+}
+
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
 });
